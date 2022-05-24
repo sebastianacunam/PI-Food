dfc789260c5b148cf58e214dbb9bf97e62e807df
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2022-05-24 15:10:23 -0300

    backend ready

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -36,7 +36,8 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT
     },
     image:{
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      defaultValue: "https://us.123rf.com/450wm/belchonock/belchonock1906/belchonock190602030/124303341-mujer-con-plato-vac%C3%ADo-en-la-mesa-de-madera-vista-superior.jpg?ver=6"
     },
     createdInDB:{
       type: DataTypes.BOOLEAN,
