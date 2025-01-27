64af288cb59d51bb1630dedf9c55a9d841cbbf59
sebastianacunam <sebasjam96@gmail.com>
2025-01-27 13:27:32 -0300

    27/01/2025, fix de la configuracion de sequelize, @sebastianacunam

--- api/src/db.js
+++ api/src/db.js
@@ -3,85 +3,60 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-//se agrega db_name para el tema del deploy
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL, NODE_ENV,
 } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
-
-// TO PRODUCTION
-let sequelize =
-  process.env.NODE_ENV === "production"
-  ? new Sequelize(`${DATABASE_URL}`,
-    { logging: false, 
-      native: false,
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      }, 
-    }
-  )
-  // ? new Sequelize({
-    //     database: DB_NAME,
-    //     dialect: "postgres",
-    //     host: DB_HOST,
-    //     port: 5432,
-    //     username: DB_USER,
-    //     password: DB_PASSWORD,
-    //     pool: {
-    //       max: 3,
-    //       min: 1,
-    //       idle: 10000,
-    //     },
-    //     dialectOptions: {
-    //       ssl: {
-    //         require: true,
-    //         // Ref.: https://github.com/brianc/node-postgres/issues/2009
-    //         rejectUnauthorized: false,
-    //       },
-    //       keepAlive: true,
-    //     },
-    //     ssl: true,
-    //   })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
-        { logging: false, native: false }
-      );
+// Configuración de Sequelize
+const sequelize = 
+  NODE_ENV === "production"
+    ? new Sequelize(DATABASE_URL, {
+        dialect: "postgres",
+        logging: false, // deshabilitar logs en producción
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // Desactiva validación estricta del certificado SSL
+          },
+        },
+      })
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+        logging: false,
+        native: false, // Permitir usar pg-native
+      });
 
 const basename = path.basename(__filename);
-
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+// Leemos todos los archivos de la carpeta Models
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .filter(
+    (file) =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+  )
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+// Injectamos la conexión (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+
+// Capitalizamos los nombres de los modelos
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map(([key, value]) => [
+  key[0].toUpperCase() + key.slice(1),
+  value,
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-
+// Relacionamos los modelos
 const { Recipe, Diet } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Recipe.belongsToMany(Diet, { through: 'recipe_diet' });
+Diet.belongsToMany(Recipe, { through: 'recipe_diet' });
 
-Recipe.belongsToMany(Diet, {through: 'recipe_diet' })
-Diet.belongsToMany(Recipe, {through: 'recipe_diet' })
-
+// Exportamos los modelos y la conexión
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // para importar los modelos
+  conn: sequelize, // para importar la conexión
 };
