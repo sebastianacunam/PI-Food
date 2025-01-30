033386556a26c21e8af42a59d07cb5a899705ca7
sebastianacunam <sebasjam96@gmail.com>
2025-01-30 11:39:18 -0300

    fix de consulta a la api y a la base de datos, para que si la api no responde por el Unauthorized, entonces busque la base de datos

--- api/src/controllers/apiInfo.js
+++ api/src/controllers/apiInfo.js
@@ -4,39 +4,29 @@ const { Recipe, Diet } = require ('../db')
 const { API_KEY } = process.env
 
 const getRecipeAPI = async () => {
-    const recipesApi = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
-    
-    const recipes = recipesApi?.data.results.map(r =>{
-        return {
-            id: r.id,
-            name: r.title,
-            resume: r.summary,
-            // rate: r.spoonacularScore,
-            healthy: r.healthScore,
-            // instructions: r.analyzedInstructions.map( i => i.steps.map( s => s.step )),
-            diets: r.diets.map( d => d),
-            image: r.image,
-        }
-    });
-    return recipes ? recipes : []
+    try {
+        const recipesApi = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
+        const recipes = recipesApi?.data.results.map(r =>{
+            return {
+                id: r.id,
+                name: r.title,
+                resume: r.summary,
+                // rate: r.spoonacularScore,
+                healthy: r.healthScore,
+                // instructions: r.analyzedInstructions.map( i => i.steps.map( s => s.step )),
+                diets: r.diets.map( d => d),
+                image: r.image,
+            }
+        });
+        return recipes ? recipes : []
+    } catch (error) {
+        return error.response.statusText
+    }
 }
 
 
-// const getRecipesDb = async () => {
-//     return await Recipe.findAll({
-//         include: {
-//             model: Diet,
-//             attributes: ['name'],
-//             through: {
-//                 attributes: [],
-//             }
-//         }
-//     })
-// }
-
 const getRecipesDbSecondTry = async () => {
     const dbInfo = await Recipe.findAll({include: Diet})
-    console.log('O ESTOY POR ACÁ? 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', dbInfo);
     const finalDbInfo = dbInfo.map( e => {
         const diets = []
         for (let i = 0; i < e.dataValues.diets ; i++){
@@ -53,16 +43,15 @@ const getRecipesDbSecondTry = async () => {
             diets: diets
         }
     })
-    // console.log(dbInfo)
     return finalDbInfo
-    // console.log(dbInfo) --> dbInfo tiene como propiedad dataValues, propiedad a la cual tengo que acceder
 }
 //---------------------------------------------------------------
 
 const getAllRecipes = async () => {
     const apiInfo = await getRecipeAPI();
     const dbInfo = await getRecipesDbSecondTry();
-
+    
+    if (apiInfo === "Unauthorized") return dbInfo; 
     const allRecipes = apiInfo.concat(dbInfo);
     return allRecipes;
 }
@@ -70,6 +59,5 @@ const getAllRecipes = async () => {
 module.exports = {
     getRecipeAPI, 
     getRecipesDbSecondTry,
-    // getRecipesDb, 
     getAllRecipes
 }
